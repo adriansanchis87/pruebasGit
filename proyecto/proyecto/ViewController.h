ac29fd2691afd2c63f4a16785abc6a5ca8d2c170
piro <piro@MacBook-Pro-de-piro.local>
2012-06-08 17:34:48 +0200

    metiendo un proyecto

---
+++ proyecto/proyecto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  proyecto
+//
+//  Created by piro on 08/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
